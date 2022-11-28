9a0515edd220ff8fec01a72151518909d1496d27
violitazahrah <violitaaditya155@gmail.com>
2022-11-28 14:53:04 +0700

    chatbot tif

--- app.py
+++ app.py
@@ -8,12 +8,48 @@ preparation()
 app = Flask(__name__)
 
 @app.route("/")
-def home():
+def index():
     return render_template("index.html")
 
-# @app.route("/home")
-# def profile():
-#     return render_template("home.html")
+@app.route("/about")
+def about():
+    return render_template("about_us.html")
+
+@app.route("/bot")
+def BOT():
+    return render_template("index.html")
+
+@app.route("/blog")
+def Blog():
+    return render_template("Blog.html")
+
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+@app.route("/Feedback")
+def feedback():
+    return render_template("Feedback.html")
+
+@app.route("/portofolio")
+def portofolio():
+    return render_template("portofolio.html")
+
+@app.route("/berita_utama")
+def beritaUtama():
+    return render_template("Berita_Utama.html")
+
+@app.route("/berita1")
+def berita1():
+    return render_template("berita1.html")
+
+@app.route("/berita2")
+def berita2():
+    return render_template("berita2.html")
+
+@app.route("/berita3")
+def berita3():
+    return render_template("berita3.html")
 
 @app.route("/get")
 def get_bot_response():
